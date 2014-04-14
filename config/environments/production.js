10d5c406801c87b67453d91ed739d0ae2ad7aa80
taoyuan <taoyuan@ollo.io>
2014-04-14 22:38:56 +0800

    added production env

---
+++ config/environments/production.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = function (maroon) {
+    var app = maroon.app;
+
+    app.setAll({
+        "debug": true,
+        "versions file": path.join(maroon.root, '/etc/opt/ollo/versions.json'),
+        "serial file": path.join(maroon.root, '/etc/opt/ollo/serial.json'),
+        "token file": path.join(maroon.root, '/etc/opt/ollo/token.json'),
+        "api": {
+            host: 'ollo.io',
+            port: 443,
+            secure: true
+        },
+        "cloud": {
+            host: 'cloud.ollo.io',
+            port: 443,
+            secure: true
+        },
+        "stream": {
+            host: 'stream.ollo.io',
+            port: 443,
+            secure: true
+        }
+    });
+
+    // config for log
+    app.set('log level', 'info');
+    app.set('log file', '/var/log/ollo.log');
+
+};
