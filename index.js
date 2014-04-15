2cd5a11d20b2d1b14b9ee850e2a251f6159c53d7
taoyuan <taoyuan@ollo.io>
2014-04-15 15:41:56 +0800

    added argv support

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ if (!process.env.NODE_ENV) {
     process.env.NODE_ENV = 'production'
 }
 
+var argv = require('optimist').usage(
+    [
+        'This process requires certain parameters to run.',
+        'Please see usage information below.',
+        '',
+        'Example: $0 --devicePath /dev/tty.usb*B'
+    ].join('\n')
+).argv;
+
 /**
  * App module exports method returning new instance of App.
  *
@@ -14,7 +23,7 @@ if (!process.env.NODE_ENV) {
  */
 var initialize = module.exports = function initialize(root) {
     root = root || __dirname;
-    require('maroon').create({}, {root: root, forward: true});
+    require('maroon').create({}, {root: root, forward: true, argv: argv});
 };
 
 if (!module.parent) {
