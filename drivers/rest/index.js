0f9f2edf43cfac9ba4066480e495572e9ab7d6a6
taoyuan <taoyuan@ollo.io>
2014-09-03 10:38:31 +0800

    Make `commands` support more format from cloud

--- drivers/rest/index.js
+++ drivers/rest/index.js
@@ -11,53 +11,53 @@ var util = require('util');
 var helpers = require('./lib/helpers');
 var devices = {};
 
-util.inherits(Rest,stream);
+util.inherits(Rest, stream);
 
 function Rest(config, ollo) {
-  var app = express();
-  var self = this;
+    var app = express();
+    var self = this;
 
-  app.configure(function(){
-    app.set('port', process.env.PORT || 8000);
-    app.use(function(req, res, next){
+    app.configure(function () {
+        app.set('port', process.env.PORT || 8000);
+        app.use(function (req, res, next) {
 
-      // Custom logger
-      self.log.info('REST %s %s', req.method, req.url);
+            // Custom logger
+            self.log.info('REST %s %s', req.method, req.url);
 
-      // Give all requests the client (for now).
-      req.ollo = ollo;
-      req.devices = devices;
-      // Keep calm and carry on
-      next();
+            // Give all requests the client (for now).
+            req.ollo = ollo;
+            req.devices = devices;
+            // Keep calm and carry on
+            next();
+        });
+
+        app.use(express.bodyParser());
+        app.use(helpers.allowCORS);
+        app.use(app.router);
     });
 
-    app.use(express.bodyParser());
-    app.use(helpers.allowCORS);
-    app.use(app.router);
-  });
+    app.get('/rest/v0/device', routes.showDevices);
+    app.get('/rest/v0/devices', routes.showDevices);
+    app.put('/rest/v0/device/:deviceGuid', routes.actuate);
+    app.post('/rest/v0/device/:deviceGuid', routes.actuate);
 
-  app.get('/rest/v0/device',routes.showDevices);
-  app.get('/rest/v0/devices',routes.showDevices);
-  app.put('/rest/v0/device/:deviceGuid',routes.actuate);
-  app.post('/rest/v0/device/:deviceGuid',routes.actuate);
+    ollo.on('device::up', function (guid) {
+        setTimeout(function () {
+            helpers.fetchDeviceData(ollo, guid, function (err, data) {
 
-  ollo.on('device::up',function(guid) {
-    setTimeout(function() {
-      helpers.fetchDeviceData(ollo, guid, function(err,data) {
+                if (err) {
+                    self.log.error('REST: %s (%s)', err.message, guid);
+                    // TODO decide what to do here
+                }
 
-        if (err) {
-          self.log.error('REST: %s (%s)', err, guid);
-          // TODO decide what to do here
-        }
+                devices[guid] = data;
+            });
+        }, 5000);
+    });
 
-        devices[guid] = data;
-      });
-    },5000);
-  });
-
-  http.createServer(app).listen(app.get('port'), function(){
-    self.log.info('Express server listening on port ' + app.get('port'));
-  });
+    http.createServer(app).listen(app.get('port'), function () {
+        self.log.info('Express server listening on port ' + app.get('port'));
+    });
 }
 
 module.exports = Rest;
--- drivers/rest/lib/helpers.js
+++ drivers/rest/lib/helpers.js
@@ -2,35 +2,35 @@ var request = require('request');
 
 exports.fetchDeviceData = function (app, guid, cb) {
 
-  var proto = (app.opts.api.secure) ? 'https://' : 'http://';
-  var uri = proto + app.opts.api.host + ':' + app.opts.api.port + '/rest/v0/device/' + guid;
-  var opts = {
-    url: uri,
-    headers: {
-      'X-Ollo-Token': app.token
-    },
-    method: 'GET',
-    json: true
-  };
+    var proto = (app.opts.api.secure) ? 'https://' : 'http://';
+    var uri = proto + app.opts.api.host + ':' + app.opts.api.port + '/rest/v0/device/' + guid;
+    var opts = {
+        url: uri,
+        headers: {
+            'X-Node-Token': app.token
+        },
+        method: 'GET',
+        json: true
+    };
 
-  request(opts, function (err, res, body) {
-    if (body && body.result === 1) {
-      cb(null, body.data)
-    } else {
-      cb(body && body.error || "REST: Unknown Error")
-    }
-  });
+    request(opts, function (err, res, body) {
+        if (body && body.result === 1) {
+            cb(null, body.data)
+        } else {
+            cb(body && body.error || "REST: Unknown Error")
+        }
+    });
 };
 
 exports.allowCORS = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.header('Origin') || '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Accept, Content-Type, Authorization, Content-Length, X-Requested-With, X-Ninja-Token, X-Ollo-Token');
+    res.header('Access-Control-Allow-Origin', req.header('Origin') || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Accept, Content-Type, Authorization, Content-Length, X-Requested-With, X-Ninja-Token, X-Ollo-Token');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' === req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
 };
